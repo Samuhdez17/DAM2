5659f824d686f2e175d66ca8ae367f832ede9f81
Samuhdez17 <lf254333@gmail.com>
2025-10-02 22:09:59 +0200

    terminado ejercicio de acc a dat

--- Acceso a Datos/src/LecturaYEscritura/Ej2_AccesoADatosEnFicheros/Main.java
+++ Acceso a Datos/src/LecturaYEscritura/Ej2_AccesoADatosEnFicheros/Main.java
@@ -41,6 +41,8 @@ public class Main {
                 case 3 -> generarFicheroConcordancia(entrada, fichero);
 
                 case 5 -> System.out.println("Saliendo....");
+
+                default -> System.out.println("Opción inválida\n");
             }
         }
     }
@@ -102,24 +104,36 @@ public class Main {
         System.out.print("Indica las aficiones del usuario separado por comas: ");
         entrada.nextLine();
         String aficiones = entrada.nextLine().toUpperCase();
-
         String[] aficionesArray = aficiones.split(",");
+
+        if (aficionesArray.length == 0) {
+            while (aficionesArray.length == 0) {
+                System.out.print("No se ha indicado ninguna afición, ingrese como mínimo una afición (o más separado por comas): ");
+                entrada.nextLine();
+                aficiones = entrada.nextLine().toUpperCase();
+                aficionesArray = aficiones.split(",");
+            }
+        }
 
         GenerarCodUsuario genCod = new GenerarCodUsuario(fichero);
 
         Usuario usuario = new Usuario(genCod.getCodigo(), aficionesArray);
 
         try (FileWriter fw = new FileWriter(fichero,  true)) {
-            fw.write(usuario + " ");
-            fw.write("\n");
+            fw.write(usuario + "\n");
 
-            System.out.println("Usuario guardada correctamente");
+            System.out.println("Usuario guardado correctamente");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     private static void leerUsuarios(File fichero) {
+        if (fichero.length() >= 1000) {
+            System.out.printf("El fichero no se puede leer porque es demasiado grande (%d bytes)\n\n", fichero.length());
+            return;
+        }
+
         try (BufferedReader br = new BufferedReader(new FileReader(fichero))) {
             String linea;
 
@@ -147,45 +161,6 @@ public class Main {
         } catch (IOException e) {}
 
         revisarConcordancias(lineas, numConcorMin);
-
-        System.out.println("Fichero generado correctamente");
-    }
-
-    private static void revisarConcordancias(ArrayList<String> lineas, int numConcorMin) {
-        for (int i = 0; i < lineas.size() ; i++) {
-            for (int j = i + 1; j < lineas.size() ; j++) {
-
-                String[] linea1 = lineas.get(i).split(" ");
-                String[] linea2 = lineas.get(j).split(" ");
-
-                // Recorre las aficiones de la segunda persona (linea2) y las compara con las de la primera (linea1)
-                ArrayList<String> comunes = new ArrayList<>();
-                for (int aficion1 = 1 ; aficion1 < linea1.length ; aficion1++) {
-                    for (int aficion2 = 1 ; aficion2 < linea2.length ; aficion2++) {
-                        if (linea1[aficion1].equals(linea2[aficion2])) {
-                            if (!comunes.contains(linea1[aficion1])) {
-                                comunes.add(linea1[aficion1]);
-                            }
-                        }
-                    }
-                }
-
-                // Si las concordancias que ha sacado son mayor o igual al mínimo indicado por el usuario, se ordenan y se escriben en un fichero de texto plano
-                if (comunes.size() >= numConcorMin) {
-                    Collections.sort(comunes);
-
-                    try (FileWriter fw = new FileWriter(CONCORDANCIAS_TXT, true)) {
-                        fw.write(String.format("%s %s ", linea1[0], linea2[0]));
-                        for (String aficion : comunes) {
-                            fw.write(aficion + " ");
-                        }
-                        fw.write("\n");
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        }
     }
 
     private static int pedirConcordanciasMinimas(Scanner entrada) {
@@ -201,6 +176,57 @@ public class Main {
             }
         }
         return numConcorMin;
+    }
+
+    private static void revisarConcordancias(ArrayList<String> lineas, int numConcorMin) {
+        int numConcordancias = 0;
+
+        for (int i = 0; i < lineas.size() ; i++) {
+            for (int j = i + 1; j < lineas.size() ; j++) {
+
+                String[] linea1 = lineas.get(i).split(" ");
+                String[] linea2 = lineas.get(j).split(" ");
+
+                ArrayList<String> comunes = encontrarConcordancias(linea1, linea2);
+
+                // Si las concordancias que ha sacado son mayor o igual al mínimo indicado por el usuario, se ordenan y se escriben en un fichero de texto plano
+                if (comunes.size() >= numConcorMin) {
+                    Collections.sort(comunes);
+
+                    try (FileWriter fw = new FileWriter(CONCORDANCIAS_TXT, true)) {
+                        fw.write(String.format("%s %s ", linea1[0], linea2[0]));
+                        for (String aficion : comunes) fw.write(aficion + " ");
+                        fw.write("\n");
+                        numConcordancias++;
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+        if (numConcordancias > 0) {
+            System.out.printf("""
+            Fichero creado correctamente.
+            Número de parejas encontradas: %d
+        """, numConcordancias);
+
+        } else System.out.println("No existen concordancias, no se ha generado el fichero");
+    }
+
+    private static ArrayList<String> encontrarConcordancias(String[] linea1, String[] linea2) {
+        // Recorre las aficiones de la segunda persona (linea2) y las compara con las de la primera (linea1)
+        ArrayList<String> comunes = new ArrayList<>();
+        for (int aficion1 = 1; aficion1 < linea1.length ; aficion1++) {
+            for (int aficion2 = 1; aficion2 < linea2.length ; aficion2++) {
+                if (linea1[aficion1].equals(linea2[aficion2])) {
+                    if (!comunes.contains(linea1[aficion1])) {
+                        comunes.add(linea1[aficion1]);
+                    }
+                }
+            }
+        }
+        return comunes;
     }
 
     private static String corregirEspacios(String nombre) {
