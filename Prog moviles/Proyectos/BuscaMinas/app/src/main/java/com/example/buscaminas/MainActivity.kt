a3517dcedb0fcab630d6b34e59f69faf94881664
Samuhdez17 <lf254333@gmail.com>
2025-10-17 14:08:49 +0200

    buscaminas in icios

--- Prog moviles/Proyectos/BuscaMinas/app/src/main/java/com/example/buscaminas/MainActivity.kt
+++ Prog moviles/Proyectos/BuscaMinas/app/src/main/java/com/example/buscaminas/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.buscaminas.ui.theme.BuscaMinasTheme
+import kotlin.math.sqrt
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +43,52 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Mapa(name: String, modifier: Modifier = Modifier) {
+    var casillas = 100.0
+    val columnas:Int = sqrt(casillas).toInt()
+    if (casillas.toInt() % columnas != 0) {
+        while (casillas.toInt() % columnas != 0) {
+            casillas += 1
+        }
+    }
+    val numBombas = 5
 
+    val posBombas = (0..casillas.toInt()).map { false }.toMutableList()
+    val posBotones = (0..casillas.toInt()).map { false }.toMutableList()
+
+    for (i in 0..posBotones.size) {
+        val posRandom = Random.nextInt()
+        if (!posBotones[posRandom]) posBombas[posRandom] = !posBotones[posRandom]
+    }
+
+    LazyVerticalGrid(
+        GridCells.Fixed(columnas),
+        modifier = Modifier.padding(3.dp),
+        horizontalArrangement = Arrangement.spacedBy(1.dp),
+        verticalArrangement = Arrangement.spacedBy(1.dp)
+        ) {
+        items(casillas.toInt()) { index ->
+            if (posBotones[index]) {
+                Button(
+                    modifier = Modifier.aspectRatio(1f).fillMaxWidth(),
+                    shape = RectangleShape,
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(53, 0, 0)
+                    )
+                ) { }
+
+            } else {
+                Button(
+                    modifier = Modifier.aspectRatio(1f).fillMaxWidth(),
+                    shape = RectangleShape,
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(53, 104, 45)
+                    )
+                ) { }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
