96f184dc8d4c75cac814a61d6206f6669cb40ab6
Samuhdez17 <lf254333@gmail.com>
2025-10-23 21:44:34 +0200

    no se

--- Servicios y procesos/Practicas/src/Ejercicios/E04carrera_100m/Carrera.java
+++ Servicios y procesos/Practicas/src/Ejercicios/E04carrera_100m/Carrera.java
@@ -1,5 +1,11 @@
 package Ejercicios.E04carrera_100m;
 
 public class Carrera implements Runnable {
+    public Carrera() {
+        super();
+    }
 
+    public void run() {
+
+    }
 }
--- Servicios y procesos/Practicas/src/Ejercicios/E04carrera_100m/Main.java
+++ Servicios y procesos/Practicas/src/Ejercicios/E04carrera_100m/Main.java
@@ -1,18 +1,43 @@
 package Ejercicios.E04carrera_100m;
 
+import java.util.Random;
+
 public class Main {
+    private static final Random RANDOM = new Random();
     public static void main(String[] args)  {
+        Carrera carrera = new Carrera();
+        Thread[] atletas = {
+                new Thread(carrera, "A"),
+                new Thread(carrera, "A"),
+                new Thread(carrera, "A"),
+                new Thread(carrera, "A"),
+                new Thread(carrera, "A"),
+                new Thread(carrera, "A"),
+                new Thread(carrera, "A"),
+                new Thread(carrera, "A")
+        };
+        asignarDorsales(atletas);
         int comienzoPrograma = (int) System.currentTimeMillis() / 1000;
 
         for (int i = 0; i < 10; i++) {
             int tiempoEnPrograma = ((int) System.currentTimeMillis() / 1000 - comienzoPrograma);
 
-            System.out.println(tiempoEnPrograma);
-
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
             }
+        }
+    }
+
+    private static void asignarDorsales(Thread[] atletas) {
+        for (Thread atleta : atletas) {
+            int dorsal = (RANDOM.nextInt(50) + 50);
+
+            while (atleta.getName().compareTo(String.valueOf(dorsal)) == 0) {
+                dorsal = (RANDOM.nextInt(50) + 50);
+            }
+
+            atleta.setName(String.valueOf(dorsal));
         }
     }
 }
