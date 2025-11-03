49b2c6305592e7d83d508b92b76584db8a37532c
Samuhdez17 <lf254333@gmail.com>
2025-11-03 21:41:33 +0100

    mas cosas

--- Prog moviles/Proyectos/BuscaMinas/app/src/main/java/com/example/buscaminas/MainActivity.kt
+++ Prog moviles/Proyectos/BuscaMinas/app/src/main/java/com/example/buscaminas/MainActivity.kt
@@ -1,23 +1,32 @@
 package com.example.buscaminas
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlin.math.sqrt
 import kotlin.random.Random
 
@@ -34,73 +43,128 @@ fun Mapa() {
         }
     }
 
+    var juegoTerminado by remember { mutableStateOf(false) }
     val botonesPulsados = (0 until casillas).map { false }.toMutableStateList()
     val posBombas = (0 until casillas).map { false }.toMutableStateList()
 
-    LazyVerticalGrid(
-    GridCells.Fixed(columnas),
-        modifier = Modifier.padding(3.dp),
-        horizontalArrangement = Arrangement.spacedBy(1.dp),
-        verticalArrangement = Arrangement.spacedBy(1.dp)
-    ) {
-        items(casillas) { index ->
-            if (botonesPulsados[index]) {
-                if (posBombas[index]) {
-                    Button(
-                        onClick = {},
-                        modifier = Modifier.aspectRatio(1f).fillMaxWidth(),
-                        shape = RectangleShape,
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color(255, 0, 0)
-                        )
-                    ) { }
-                } else {
-                    val bombasCerca = mirarAlrededor(posBombas, index, columnas)
+    Column() {
+        Text(
+            "BUSCAMINAS",
+            modifier = Modifier.padding(3.dp).align(Alignment.CenterHorizontally),
+            fontSize = (24.sp),
+        )
 
-                    Button(
-                        onClick = {},
-                        modifier = Modifier.aspectRatio(1f).fillMaxWidth(),
-                        contentPadding = PaddingValues(0.dp),
-                        shape = RectangleShape,
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color(255, 255, 255)
-                        )
-                    ) {
-                        when(bombasCerca) {
-                            1 -> Text(
-                                text = bombasCerca.toString(),
-                                color = Color(0, 0, 0)
-                            )
-                            2 -> Text(
-                                text = bombasCerca.toString(),
-                                color = Color(20, 200, 30)
-                            )
-                            3 -> Text(
-                                text = bombasCerca.toString(),
-                                color = Color(250, 0, 0)
-                            )
-                            4 -> Text(
-                                text = bombasCerca.toString(),
-                                color = Color(50, 0, 140)
-                            )
-                            0 -> {}
-                        }
-                    }
+        var textoEstado by remember { mutableStateOf("Pulsa una casilla para empezar") }
+
+        Text(
+            textoEstado,
+            modifier = Modifier.padding(3.dp).align(Alignment.CenterHorizontally),
+        )
+
+        Button(
+            onClick = {
+                juegoTerminado = false
+                for (i in 0 until casillas) {
+                    botonesPulsados[i] = false
+                    posBombas[i] = false
                 }
-            } else {
-                Button(
-                    onClick = {
-                        if (partidaSinEmpezar(botonesPulsados)) colocarBombas(posBombas, index)
-                        botonesPulsados[index] = true
-                    },
-                    modifier = Modifier.aspectRatio(1f).fillMaxWidth(),
-                    shape = RectangleShape,
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(53, 104, 45)
-                    )
-                ) { }
-            }
+            },
+            modifier = Modifier
+                .padding(6.dp)
+                .align(Alignment.CenterHorizontally),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(100, 100, 100))
+        ) {
+            Text("Reiniciar", color = Color.White)
         }
+
+            LazyVerticalGrid(
+                GridCells.Fixed(columnas),
+                modifier = Modifier.padding(3.dp),
+                horizontalArrangement = Arrangement.spacedBy(1.dp),
+                verticalArrangement = Arrangement.spacedBy(1.dp)
+            ) {
+                items(casillas) { index ->
+                    if (botonesPulsados[index]) {
+                        if (posBombas[index]) {
+                            Button(
+                                onClick = {},
+                                modifier = Modifier.aspectRatio(1f).fillMaxWidth(),
+                                shape = RectangleShape,
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(255, 0, 0)
+                                )
+                            ) { }
+
+                            juegoTerminado = true
+                            for (pos in 0 until posBombas.size) {
+                                if (posBombas[pos]) botonesPulsados[pos] = true
+                            }
+
+                            textoEstado = "Has perdido"
+
+                        } else {
+                            val bombasCerca = mirarAlrededor(posBombas, index, columnas)
+
+                            Button(
+                                onClick = {},
+                                modifier = Modifier.aspectRatio(1f).fillMaxWidth(),
+                                contentPadding = PaddingValues(0.dp),
+                                shape = RectangleShape,
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(255, 255, 255)
+                                )
+                            ) {
+                                when (bombasCerca) {
+                                    1 -> Text(
+                                        text = bombasCerca.toString(),
+                                        color = Color(0, 0, 0)
+                                    )
+
+                                    2 -> Text(
+                                        text = bombasCerca.toString(),
+                                        color = Color(20, 200, 30)
+                                    )
+
+                                    3 -> Text(
+                                        text = bombasCerca.toString(),
+                                        color = Color(250, 0, 0)
+                                    )
+
+                                    4 -> Text(
+                                        text = bombasCerca.toString(),
+                                        color = Color(50, 0, 140)
+                                    )
+
+                                    0 -> {}
+                                }
+                            }
+                        }
+                    } else {
+                        Button(
+                            onClick = {
+                                if (!juegoTerminado) {
+                                    if (partidaSinEmpezar(botonesPulsados)) {
+                                        colocarBombas(
+                                            posBombas,
+                                            index
+                                        )
+
+                                        textoEstado = "Pulsa una casilla para descubrir"
+                                    }
+                                    botonesPulsados[index] = true
+                                }
+                            },
+                            modifier = Modifier.aspectRatio(1f).fillMaxWidth(),
+                            shape = RectangleShape,
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(53, 104, 45)
+                            )
+                        ) { }
+                    }
+
+
+                }
+            }
     }
 }
 
