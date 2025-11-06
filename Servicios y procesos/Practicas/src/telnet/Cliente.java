3021f18622d426c36b90b38095f0a4afff1253fb
Samuhdez17 <lf254333@gmail.com>
2025-11-06 14:05:31 +0100

    socket

---
+++ Servicios y procesos/Practicas/src/telnet/Cliente.java
@@ -0,0 +1,34 @@
+package telnet;
+
+import java.io.*;
+import java.net.*;
+
+public class Cliente {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int puerto = 5000;
+
+        try (Socket socket = new Socket(host, puerto)) {
+            System.out.println("Conectado al servidor.");
+            String mensaje = "";
+
+            do {
+            // Flujos de entrada/salida
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+//            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            System.out.print("Escribe un mensaje para el servidor: ");
+            mensaje = teclado.readLine();
+
+            salida.println(mensaje);  // Enviar mensaje al servidor
+            } while (!mensaje.equals("salir"));
+//            String respuesta = entrada.readLine();  // Leer respuesta del servidor
+//            System.out.println("Servidor responde: " + respuesta);
+
+        } catch (IOException e) {
+            System.out.println("Error en el cliente: " + e.getMessage());
+        }
+    }
+}
+
---
+++ Servicios y procesos/Practicas/src/telnet/Servidor.java
@@ -0,0 +1,41 @@
+package telnet;
+
+import java.io.*;
+import java.net.*;
+
+public class Servidor {
+    public static void main(String[] args) {
+        int puerto = 5000;
+
+        try (ServerSocket servidor = new ServerSocket(puerto)) {
+            Socket cliente = null;
+            String mensaje = "";
+
+            System.out.println("Servidor iniciado. Esperando conexión en el puerto " + puerto + "...");
+
+            // Espera hasta que un cliente se conecte
+            cliente = servidor.accept();
+            System.out.println("Cliente conectado desde: " + cliente.getInetAddress().getHostAddress());
+
+            do {
+            // Flujos de entrada/salida
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
+
+            // Comunicación
+            mensaje = entrada.readLine();
+            System.out.println("Cliente dice: " + mensaje);
+
+            salida.println("Mensaje recibido: " + mensaje.toUpperCase());
+            } while (!mensaje.equals("salir"));
+
+            // Cierre
+            cliente.close();
+            System.out.println("Conexión cerrada con el cliente.");
+
+        } catch (IOException e) {
+            System.out.println("Error en el servidor: " + e.getMessage());
+        }
+    }
+}
+
