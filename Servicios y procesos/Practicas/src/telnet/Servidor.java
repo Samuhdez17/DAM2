41704ed68c52e435c2af366032db81895b832c6a
Samuhdez17 <lf254333@gmail.com>
2025-11-06 19:15:30 +0100

    servidor siempre oyente

--- Servicios y procesos/Practicas/src/telnet/Servidor.java
+++ Servicios y procesos/Practicas/src/telnet/Servidor.java
@@ -11,27 +11,29 @@ public class Servidor {
             Socket cliente = null;
             String mensaje = "";
 
-            System.out.println("Servidor iniciado. Esperando conexión en el puerto " + puerto + "...");
+            while (true) {
+                System.out.println("Servidor iniciado. Esperando conexión en el puerto " + puerto + "...");
 
-            // Espera hasta que un cliente se conecte
-            cliente = servidor.accept();
-            System.out.println("Cliente conectado desde: " + cliente.getInetAddress().getHostAddress());
+                // Espera hasta que un cliente se conecte
+                cliente = servidor.accept();
+                System.out.println("Cliente conectado desde: " + cliente.getInetAddress().getHostAddress());
 
-            do {
-            // Flujos de entrada/salida
-            BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-            PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
+                do {
+                    // Flujos de entrada/salida
+                    BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+                    PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
 
-            // Comunicación
-            mensaje = entrada.readLine();
-            System.out.println("Cliente dice: " + mensaje);
+                    // Comunicación
+                    mensaje = entrada.readLine();
+                    System.out.println("Cliente dice: " + mensaje);
 
-            salida.println("Mensaje recibido: " + mensaje.toUpperCase());
-            } while (!mensaje.equals("salir"));
+                    salida.println("Mensaje recibido: " + mensaje);
+                } while (!mensaje.equals("salir"));
 
-            // Cierre
-            cliente.close();
-            System.out.println("Conexión cerrada con el cliente.");
+                // Cierre
+                cliente.close();
+                System.out.println("Conexión cerrada con el cliente.");
+            }
 
         } catch (IOException e) {
             System.out.println("Error en el servidor: " + e.getMessage());
