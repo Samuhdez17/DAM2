263c2e81c99a73d8e007dadc925f14d21c8dbc61
Samuhdez17 <lf254333@gmail.com>
2025-11-28 10:15:07 +0100

    se guardan correctamente los registros en el HashMap, queda creear el servidor

--- Servicios y procesos/Practicas/src/Ejercicios/E009DNS/Main.java
+++ Servicios y procesos/Practicas/src/Ejercicios/E009DNS/Main.java
@@ -1,4 +1,46 @@
 package Ejercicios.E009DNS;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
+    private final static String RUTA_TXT = "dominios";
+    private final static int PUERTO = 5000;
+
+    public static void main(String[] args) throws IOException {
+        File ficheroDominios = new File(RUTA_TXT);
+        HashMap<String, ArrayList<Registro>> registros = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ficheroDominios))) {
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                String[] contenidoRegistro = linea.split(" ");
+
+                registros.putIfAbsent(contenidoRegistro[0], new ArrayList<>());
+
+                ArrayList<Registro> registrosDelMismoDominio = registros.get(contenidoRegistro[0]);
+                registrosDelMismoDominio.add(new Registro(contenidoRegistro));
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error al leer el fichero");
+            throw new RuntimeException(e);
+        }
+
+        ServerSocket serverSocket = new ServerSocket(PUERTO);
+        System.out.println("Servidor iniciado en el puerto " + PUERTO);
+        Socket socket = serverSocket.accept();
+
+        ArrayList<Registro> prueba = registros.get("google.com");
+        for (Registro r : prueba) {
+            System.out.println(r);
+        }
+    }
 }
