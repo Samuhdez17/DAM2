9a2d2317034eb207c160a5db76d0f7e07a337dab
Samuhdez17 <lf254333@gmail.com>
2025-12-01 19:58:11 +0100

    slider bar segunda parte

--- Interfaces/JavaFX/t4/ejercicios/T4_E006_SliderBar/src/main/java/module-info.java
+++ Interfaces/JavaFX/t4/ejercicios/T4_E006_SliderBar/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.t4_e006_sliderbar {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens org.example.t4_e006_sliderbar to javafx.fxml;
--- Interfaces/JavaFX/t4/ejercicios/T4_E006_SliderBar/src/main/java/org/example/t4_e006_sliderbar/Controlador.java
+++ Interfaces/JavaFX/t4/ejercicios/T4_E006_SliderBar/src/main/java/org/example/t4_e006_sliderbar/Controlador.java
@@ -3,79 +3,236 @@ package org.example.t4_e006_sliderbar;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.Random;
 
 public class Controlador {
-
-    @FXML private Label labelBlue;
-    @FXML private Pane paneColor;
-    @FXML private Label labelGreen;
-    @FXML private Label labelOpacity;
-    @FXML private Label labelRed;
-
-    @FXML private Slider sliderBlue;
-    @FXML private Slider sliderGreen;
-    @FXML private Slider sliderOpacity;
-    @FXML private Slider sliderRed;
-
-    @FXML private TextField valueBlue;
-    @FXML private TextField valueGreen;
-    @FXML private TextField valueOpacity;
-    @FXML private TextField valueRed;
+    @FXML
+    private Slider barraAzul;
 
     @FXML
-    void cambioSlider(MouseEvent event) {
-        if (event.getSource() == sliderRed) {
-            valueRed.setText(String.format("%.0f", sliderRed.getValue()));
-        } else if (event.getSource() == sliderGreen) {
-            valueGreen.setText(String.format("%.0f", sliderGreen.getValue()));
-        } else if (event.getSource() == sliderBlue) {
-            valueBlue.setText(String.format("%.0f", sliderBlue.getValue()));
-        } else if (event.getSource() == sliderOpacity) {
-            valueOpacity.setText(String.format("%.2f", sliderOpacity.getValue()));
+    private Slider barraOpacidad;
+
+    @FXML
+    private Slider barraRojo;
+
+    @FXML
+    private Slider barraVerde;
+
+    @FXML
+    private Pane paneColor;
+
+    @FXML
+    private TextField valorAzul;
+
+    @FXML
+    private TextField valorOpacidad;
+
+    @FXML
+    private TextField valorRojo;
+
+    @FXML
+    private TextField valorVerde;
+
+    @FXML
+    private MenuBar menuBar;
+
+    // FILE
+    @FXML
+    private Menu menuFile;
+
+    @FXML
+    private MenuItem loadItem;
+
+    @FXML
+    private MenuItem saveItem;
+
+    // EDIT
+    @FXML
+    private MenuItem resetItem;
+
+    @FXML
+    private MenuItem randomItem;
+
+    @FXML
+    private MenuItem cyanItem;
+
+    @FXML
+    private MenuItem magentaItem;
+
+    @FXML
+    private MenuItem yellowItem;
+
+    @FXML
+    private MenuItem blackItem;
+
+    @FXML
+    private MenuItem whiteItem;
+
+    @FXML
+    private MenuItem orangeItem;
+
+    @FXML
+    private MenuItem brownItem;
+
+    @FXML
+    private MenuItem pinkItem;
+
+    // HELP
+    @FXML
+    private MenuItem aboutItem;
+
+    @FXML
+    void initialize() {
+        if (menuBar != null) {
+            // MENU FILE
+            SeparatorMenuItem sep = new SeparatorMenuItem();
+            menuFile.getItems().add( 2, sep);
+            loadItem.setAccelerator(KeyCombination.keyCombination("Ctrl+L"));
+            saveItem.setAccelerator(KeyCombination.keyCombination("Ctrl+S"));
+
+            // MENU EDIT
+            resetItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Alt+0"));
+            randomItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Alt+A"));
+            cyanItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Alt+C"));
+            magentaItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Alt+N"));
+            yellowItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Alt+Y"));
+            blackItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Alt+1"));
+            whiteItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Alt+2"));
+            orangeItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Alt+3"));
+            brownItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Alt+4"));
+            pinkItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Alt+5"));
         }
+    }
+
+    @FXML
+    void cambioSlider(MouseEvent e) {
+             if (e.getSource() == barraRojo)        valorRojo.setText(String.format("%.0f", barraRojo.getValue()));
+        else if (e.getSource() == barraVerde)       valorVerde.setText(String.format("%.0f", barraVerde.getValue()));
+        else if (e.getSource() == barraAzul)        valorAzul.setText(String.format("%.0f", barraAzul.getValue()));
+        else if (e.getSource() == barraOpacidad)    valorOpacidad.setText(String.format("%.2f", barraOpacidad.getValue()));
+
         actualizarColor(
-                (int) sliderRed.getValue(),
-                (int) sliderGreen.getValue(),
-                (int) sliderBlue.getValue(),
-                sliderOpacity.getValue()
+                (int) barraRojo.getValue(),
+                (int) barraVerde.getValue(),
+                (int) barraAzul.getValue(),
+                barraOpacidad.getValue()
         );
     }
 
     @FXML
-    void cambioValor(ActionEvent event) {
+    void cambioValor(ActionEvent e) {
         try {
-            if (event.getSource() == valueRed) {
-                sliderRed.setValue(Double.parseDouble(valueRed.getText()));
-            } else if (event.getSource() == valueGreen) {
-                sliderGreen.setValue(Double.parseDouble(valueGreen.getText()));
-            } else if (event.getSource() == valueBlue) {
-                sliderBlue.setValue(Double.parseDouble(valueBlue.getText()));
-            } else if (event.getSource() == valueOpacity) {
-                sliderOpacity.setValue(Double.parseDouble(valueOpacity.getText()));
-            }
-        } catch (NumberFormatException e) {
+                 if (e.getSource() == valorRojo)        barraRojo.setValue(Double.parseDouble(valorRojo.getText()));
+            else if (e.getSource() == valorVerde)       barraVerde.setValue(Double.parseDouble(valorVerde.getText()));
+            else if (e.getSource() == valorAzul)        barraAzul.setValue(Double.parseDouble(valorAzul.getText()));
+            else if (e.getSource() == valorOpacidad)    barraOpacidad.setValue(Double.parseDouble(valorOpacidad.getText()));
+
+        } catch (NumberFormatException ex) {
         }
 
         actualizarColor(
-                (int) sliderRed.getValue(),
-                (int) sliderGreen.getValue(),
-                (int) sliderBlue.getValue(),
-                sliderOpacity.getValue()
+                (int) barraRojo.getValue(),
+                (int) barraVerde.getValue(),
+                (int) barraAzul.getValue(),
+                barraOpacidad.getValue()
         );
     }
 
     private void actualizarColor(int r, int g, int b, double a) {
-        Color newColor = Color.rgb(r ,g ,b , a);
-        BackgroundFill fill = new BackgroundFill(newColor, CornerRadii.EMPTY, Insets.EMPTY);
+        Color colorNuevo = Color.rgb(r, g, b, a);
+        BackgroundFill fill = new BackgroundFill(colorNuevo, CornerRadii.EMPTY, Insets.EMPTY);
         paneColor.setBackground(new Background(fill));
+    }
+
+    private void setValores(int r, int g, int b, double a) {
+        barraRojo.setValue(r);
+        barraVerde.setValue(g);
+        barraAzul.setValue(b);
+        barraOpacidad.setValue(a);
+
+        valorRojo.setText(String.format("%d", r));
+        valorVerde.setText(String.format("%d", g));
+        valorAzul.setText(String.format("%d", b));
+        valorOpacidad.setText(String.format("%.2f", a));
+    }
+
+    @FXML
+    void colorRandom(ActionEvent e) {
+        Random ran = new Random();
+
+        int r = ran.nextInt(256);
+        int g = ran.nextInt(256);
+        int b = ran.nextInt(256);
+        double a = ran.nextDouble(1);
+
+        setValores(r, g, b, a);
+        actualizarColor(r, g, b, a);
+    }
+
+    @FXML
+    void resetearColor(ActionEvent e) {
+        setValores(0, 0, 0, 1);
+        actualizarColor(0, 0, 0, 1);
+    }
+
+    @FXML
+    void ponerColor(ActionEvent e) {
+        int r = 0;
+        int g = 0;
+        int b = 0;
+
+        if (e.getSource() == cyanItem) {
+            r = 0; g = 255; b = 255;
+
+        }  else if (e.getSource() == magentaItem) {
+            r = 255; g = 0; b = 255;
+
+        } else if (e.getSource() == yellowItem) {
+            r = 255; g = 255; b = 0;
+
+        } else if (e.getSource() == blackItem) {
+            r = 0; g = 0; b = 0;
+
+        } else if (e.getSource() == whiteItem) {
+            r = 255; g = 255; b = 255;
+
+        } else if (e.getSource() == orangeItem) {
+            r = 255; g = 165; b = 0;
+
+        } else if (e.getSource() == brownItem) {
+            r = 128; g = 64; b = 0;
+
+        } else if (e.getSource() == pinkItem) {
+            r = 255; g = 192; b = 203;
+        }
+
+        setValores(r, g, b, 1.00);
+        actualizarColor(r, g, b, 1.00);
+    }
+
+    @FXML
+    void generarVentana(ActionEvent event) {
+        Label contenido = new Label("DAM2 ©2025");
+        Button boton = new Button("OK");
+        VBox vBox = new VBox(10);
+        vBox.getChildren().addAll(contenido, boton);
+        vBox.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(vBox, 150, 100);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+        boton.setOnAction(e -> stage.close());
     }
 }
