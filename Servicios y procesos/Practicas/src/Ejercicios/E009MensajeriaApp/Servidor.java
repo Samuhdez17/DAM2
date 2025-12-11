241b77edca8dba3040b4fb7dd0e82e461fc40c57
Samuhdez17 <lf254333@gmail.com>
2025-12-11 21:50:00 +0100

    continuaciones de syp e interfaces

--- Servicios y procesos/Practicas/src/Ejercicios/E009MensajeriaApp/Servidor.java
+++ Servicios y procesos/Practicas/src/Ejercicios/E009MensajeriaApp/Servidor.java
@@ -3,10 +3,14 @@ package Ejercicios.E009MensajeriaApp;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Servidor {
     private final static int PUERTO = 5000;
+    private final static String RUTA_BUZON = "src/Ejercicios/E009MensajeriaApp/buzon";
 
     public static void main(String[] args) throws IOException {
         ServerSocket servidor = new ServerSocket(PUERTO);
@@ -16,14 +20,37 @@ public class Servidor {
         System.out.println("Cliente conectado desde: " + cliente.getInetAddress());
 
         BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-        PrintWriter salida = new PrintWriter(new OutputStreamWriter(cliente.getOutputStream()));
+        PrintWriter salida = new PrintWriter(new OutputStreamWriter(cliente.getOutputStream()), true);
 
-        salida.print("Bienvenido, por favor, indica tu nombre: ");
-        entrada.readLine();
-        salida.println();
+        salida.println("Bienvenido, por favor, indica tu nombre: ");
+        String nombre = entrada.readLine();
 
         String comandoCliente = entrada.readLine();
         if (comandoCliente.equals("leer")) {
+            DataInputStream dis = new DataInputStream(new FileInputStream(RUTA_BUZON));
+            List<Mensaje> mensajes = new ArrayList<>();
+
+            try {
+                while (true) {
+                    String[] linea = dis.readUTF().split("``");
+                    if (linea.length <= 3 && linea[0].equals(nombre))
+                        mensajes.add(new Mensaje(linea));
+                }
+
+            } catch (EOFException e) {
+
+            }
+
+            if (mensajes != null && !mensajes.isEmpty()) {
+                salida.println("150 Inicio listado");
+                for (Mensaje m : mensajes)
+                    salida.printf("De: %s ; Mensaje %s\n", m.getEmisor(), m.getContenido());
+
+                salida.println("226 Fin listado");
+
+            } else {
+                salida.println("404 No tienes mensajes");
+            }
 
         } else if (comandoCliente.equals("enviar")) {
 
